bf9072669de301d86e55327cf88fdd38c497fb8d
Fahad Qazi <fahadqazi@gmail.com>
2017-08-02 21:40:55 +0100

    another exmaple of trail call recursion

--- index.js
+++ index.js
@@ -8,5 +8,18 @@ const factorIt = (number, acc) => {
   }
 }
 
-const result = factorialPTC(6)
-console.log(result);
+console.log(factorialPTC(5));
+
+//Another example - downward sum of numbers 
+
+const sumPTC = number => sum(number, 0);
+
+const sum = (number, acc) => {
+  if(number<1){
+    return acc
+  }else{
+    return sum(number - 1, number + acc)
+  }
+}
+
+console.log(sumPTC(5));
